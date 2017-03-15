d7dc357bbc5b6acdb9d811571e724b54621826e5
Mohamed Elfiky <mohamed.elfikyy@yahoo.com>
2017-03-15 23:37:58 +0200

    linked list new features and fix remove head

--- data_structures/linked_list/linked_list.go
+++ data_structures/linked_list/linked_list.go
@@ -1,20 +1,20 @@
 package linked_list
 
 type ListNode struct {
-	value interface{}
+	Value interface{}
 	next  *ListNode
 }
 
 type LinkedList struct {
-	head *ListNode
+	Head *ListNode
 }
 
 func (l *LinkedList) Push(value interface{}) *ListNode {
-	node := &ListNode{value: value}
-	if l.head == nil {
-		l.head = node
+	node := &ListNode{Value: value}
+	if l.IsEmpty() {
+		l.Head = node
 	} else {
-		last := &(l.head)
+		last := &(l.Head)
 		for (*last).next != nil {
 			last = &((*last).next)
 		}
@@ -24,38 +24,65 @@ func (l *LinkedList) Push(value interface{}) *ListNode {
 }
 
 func (l *LinkedList) Insert(afterMe *ListNode, value interface{}) *ListNode {
-	node := &ListNode{value: value}
+	node := &ListNode{Value: value}
 	node.next = afterMe.next
 	afterMe.next = node
 	return node
 }
 
 func (l *LinkedList) Search(value interface{}) *ListNode {
-	if l.head != nil {
-		current := &(l.head)
-		for *current != nil {
-			if (*current).value == value {
-				return *current
+	if !l.IsEmpty() {
+		return l.SearchBy(func(item interface{}) bool {
+			return item.(*ListNode).Value == value
+		})
+	}
+	return nil
+}
+
+func (l *LinkedList) SearchBy(comparer func(item interface{}) bool) *ListNode {
+	if !l.IsEmpty() {
+		current := l.Head
+		for current != nil {
+			if comparer(current) {
+				return current
 			}
-			current = &((*current).next)
+			current = current.next
 		}
 	}
 	return nil
 }
 
 func (l *LinkedList) PushFirst(value interface{}) {
-	l.head = &ListNode{value: value, next: l.head}
+	l.Head = &ListNode{Value: value, next: l.Head}
+}
+
+func (l *LinkedList) IsEmpty() bool {
+	return l.Head == nil
+}
+
+func (n *ListNode) Next() *ListNode {
+	return n.next
 }
 
 func (l *LinkedList) Remove(item *ListNode) {
-	if l.head != nil {
-		current := &(l.head)
-		for *current != nil {
-			if (*current).next == item {
-				(*current).next = (*current).next.next
+	if !l.IsEmpty() {
+		if l.Head == item {
+			l.Head = l.Head.next
+			return
+		}
+		current := l.Head
+		for current != nil {
+			if current.next == item {
+				current.RemoveNext()
 				break
 			}
-			current = &((*current).next)
+			current = current.next
 		}
+	}
+}
+
+func (n *ListNode) RemoveNext() {
+	if n != nil && n.next != nil {
+		n.next = n.next.next
 	}
 }
