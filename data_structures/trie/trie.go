4a395a734c03982139a9a636460cf58811240408
Mohamed Elfiky <mohamed.elfikyy@yahoo.com>
2017-03-18 13:06:12 +0200

    trie test cases

--- data_structures/trie/trie.go
+++ data_structures/trie/trie.go
@@ -5,7 +5,15 @@ type Trie struct {
 	isLeaf   bool
 }
 
-func (t *Trie) GetChildren() map[byte]*Trie { return t.children }
+func (t *Trie) GetChildrenNodes() map[byte]*Trie { return t.children }
+func (t *Trie) GetChildren() []string {
+	res, i := make([]string, len(t.children)), 0
+	for k, _ := range t.children {
+		res[i] = string(k)
+		i++
+	}
+	return res
+}
 
 func NewTrie() *Trie {
 	return &Trie{children: make(map[byte]*Trie)}
@@ -31,7 +39,7 @@ func (t *Trie) WordExist(word string) bool {
 	for i := 0; i < len(word); i++ {
 		if tr, ok := t.children[word[i]]; ok {
 			t = tr
-			if t.isLeaf {
+			if t.isLeaf && len(word) == i+1 {
 				return true
 			}
 		} else {
